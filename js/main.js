20ae4ce63cc577cf0c4420188d078f1c893ec2bf
fabo <lopezfabrizio@ymail.com>
2020-02-03 19:38:51 -0600

    completed overall game logic

--- js/main.js
+++ js/main.js
@@ -24,6 +24,7 @@ const messageEle = document.getElementById('message');
 
 /*----- event listeners -----*/
 document.getElementById('grid-container').addEventListener('click', handleClick);
+document.getElementById('reset').addEventListener('click', init); //says when Reset Board is clicked, call init(which starts the code over)
 
 /*----- functions -----*/
 
@@ -32,16 +33,20 @@ function init() {
     board = new Array(9).fill(null); // same as [null, null, null, null, null, null, null, null, null];
     turn = 1;
     winner = false;
+    
+    
     render();
 }
 
 init();
 
-function handleClick(e) {
 
-    console.log(e.target.dataset.index);
+function handleClick(e) {
     let idx = e.target.dataset.index;
+    if (winner || board[idx]) return;
     board[idx] = turn;
+    
+    
     // toggle turn
     turn *= -1;
     // check if winner
@@ -51,17 +56,36 @@ function handleClick(e) {
 
 function checkWinner() {
     // write logic to determine winner
-    // return true or false
-    render();
+    
+    for (let i = 0; i < COMBOS.length; i++) {
+        if (Math.abs(board[COMBOS[i][0]] + board[COMBOS[i][1]] + board[COMBOS[i][2]]) === 3) return board[COMBOS[i][0]];
+        // ^ grabbing the absolute value of each value of combos, either 1 or -1, and sees if they add up to 3
+    };
+    if (board.includes(null)) return false; // checks if there is any empty space on the board
+    return 'T'; //if none, return T for tie
 }
 
 function render() {
     // draws the game board
     board.forEach(function(mark, index){
         gridEle[index].textContent = KEY[mark];
-        console.log('Board: ', board);
-        console.log('Turn: ', turn);
+        message();
     });
     // transfer the state of the app to the DOM
     // what data do we use to draw the board?
+    
 }
+
+function message() {
+    if (!winner) {
+        messageEle.textContent = `${KEY[turn]}'s Turn!`;
+        messageEle.style.color = 'white';
+    } else if (winner === 'T') {
+        messageEle.textContent = "It's a tie!";
+        messageEle.style.color = "gold";
+    } else {
+        messageEle.textContent = `${KEY[winner]} Wins!`;
+        messageEle.style.color = 'aquamarine';
+    }
+}
+
